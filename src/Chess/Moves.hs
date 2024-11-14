af29b3abcc54101e49dc604596779ed34a73f196
ryndubei <114586905+ryndubei@users.noreply.github.com>
2024-11-14 15:06:36 +0000

    Rewrite all current moves into the Move GADT

--- src/Chess/Moves.hs
+++ src/Chess/Moves.hs
@@ -3,15 +3,11 @@
 {-# LANGUAGE RequiredTypeArguments #-}
 {-# LANGUAGE RoleAnnotations #-}
 {-# LANGUAGE ImpredicativeTypes #-}
+{-# LANGUAGE StrictData #-}
 
 module Chess.Moves
   ( Board
   , initialBoard
-  , movePawn1
-  , movePawn2
-  , capturePawn
-  , moveKing
-  , captureKing
   , Unthreatened'(..)
   , Threatened'(..)
   , Move(..)
@@ -22,7 +18,6 @@ module Chess.Moves
 
 import Chess.Types
 import Prelude.Singletons
-import Data.Kind
 import Data.Singletons.Base.Enum
 
 import Common.TypeOr
@@ -36,31 +31,6 @@ data Board (facts :: FactSet) = Board
 type role Board nominal
 
 type HasKing = HasPiece King
-
-type Unthreatened (cell :: Cell) (by :: Colour) (facts :: FactSet) =
-  ( UnthreatenedBy Knight cell by facts
-  , UnthreatenedBy Bishop cell by facts
-  , UnthreatenedBy Rook cell by facts
-  , UnthreatenedBy Pawn cell by facts
-  , UnthreatenedBy Queen cell by facts
-  -- Don't need to check this except when moving the king:
-  -- , UnthreatenedByKing cell by facts
-  )
-
--- TODO
-type family UnthreatenedBy (piece :: Piece) (cell :: Cell) (by :: Colour) (facts :: FactSet) :: Constraint where
-  UnthreatenedBy Knight cell by facts = ()
-  UnthreatenedBy Bishop cell by facts = ()
-  UnthreatenedBy Rook cell by facts = ()
-  UnthreatenedBy Pawn cell by facts = ()
-  UnthreatenedBy Queen cell by facts = ()
-
-type UnthreatenedByKing cell by facts = () :: Constraint
-
-type ReallyUnthreatened (cell :: Cell) (by :: Colour) (facts :: FactSet) =
-  ( Unthreatened cell by facts
-  , UnthreatenedByKing cell by facts
-  )
 
 -- Helpers for initialBoard
 type CellsOfVertical = ((F.Flip F.Map) (EnumFromTo MinBound MaxBound) F.<=< F.Pure1 (F.Flip (F.Pure2 'Cell)))
@@ -101,7 +71,77 @@ data Move (colour :: Colour) (moveFrom :: Cell) (moveTo :: Cell) (facts :: FactS
                )
             => Unthreatened' kingCell (Opponent colour) facts'
             -> Move colour moveFrom moveTo facts facts'
- 
+  MovePawn2 :: ( Holds (HasPiece Pawn colour moveFrom) facts
+               , Holds (IsEmpty moveTo) facts
+               , moveFrom ~ 'Cell hFrom (PawnRank colour)
+               , moveTo ~ 'Cell hFrom vTo
+               , vTo ~ Forward colour (Forward colour (PawnRank colour))
+               , facts' ~ DeleteInsert
+                   [IsEmpty moveTo, HasPiece Pawn colour moveFrom]
+                   [IsEmpty moveFrom, HasPiece Pawn colour moveTo]
+                   facts
+               , Holds (HasKing colour kingCell) facts
+               )
+            => Unthreatened' kingCell (Opponent colour) facts'
+            -> Move colour moveFrom moveTo facts facts'
+  CapturePawn :: ( Holds (HasPiece Pawn colour moveFrom) facts
+                 , Holds (HasPiece opponentPiece (Opponent colour) moveTo) facts
+                 , moveFrom ~ 'Cell hFrom vFrom
+                 , moveTo ~ 'Cell hTo vTo
+                 , vTo ~ Forward colour vFrom
+                 -- Note that we now use an explicit disjunction constraint
+                 -- (a, b \/ c) => t
+                 -- instead of
+                 -- a => (b => t, c => t)
+                 -- as in Frog
+                 , hTo ~ Rightward hFrom \/ hTo ~ Leftward hFrom
+                 , facts' ~ DeleteInsert
+                     [HasPiece opponentPiece (Opponent colour) moveTo, HasPiece Pawn colour moveFrom]
+                     [IsEmpty moveFrom, HasPiece Pawn colour moveTo]
+                     facts
+                 , Holds (HasKing colour kingCell) facts
+                 )
+              => Unthreatened' kingCell (Opponent colour) facts'
+              -> Proxy opponentPiece -- aughhhhh no visible forall in GADTs yet, see GHC issue !25127 
+              -> Move colour moveFrom moveTo facts facts'
+  MoveKing :: ( Holds (HasKing colour moveFrom) facts
+              , Holds (IsEmpty moveTo) facts
+              , moveFrom ~ 'Cell hFrom vFrom
+              , moveTo ~ 'Cell hTo vTo
+              , facts' ~ DeleteInsert
+                  [IsEmpty moveTo, HasKing colour moveFrom]
+                  [IsEmpty moveFrom, HasKing colour moveTo]
+                  facts
+              , ( (hTo ~ Succ hFrom \/ hTo ~ Pred hFrom)
+                , (vTo ~ vFrom \/ vTo ~ Succ vFrom \/ vTo ~ Pred vFrom)
+                )
+                \/
+                ( (hTo ~ hFrom \/ hTo ~ Succ hFrom \/ hTo ~ Pred hFrom)
+                , (vTo ~ Succ vFrom \/ vTo ~ Pred vFrom)
+                )
+              )
+           => Unthreatened' kingCell (Opponent colour) facts'
+           -> Move colour moveFrom moveTo facts facts'
+  CaptureKing :: ( Holds (HasKing colour moveFrom) facts
+                 , Holds (HasPiece opponentPiece (Opponent colour) moveTo) facts
+                 , moveFrom ~ 'Cell hFrom vFrom
+                 , moveTo ~ 'Cell hTo vTo
+                 , facts' ~ DeleteInsert
+                     [HasPiece opponentPiece (Opponent colour) moveTo, HasKing colour moveFrom]
+                     [IsEmpty moveFrom, HasKing colour moveTo]
+                     facts
+                 , ( (hTo ~ Succ hFrom \/ hTo ~ Pred hFrom)
+                   , (vTo ~ vFrom \/ vTo ~ Succ vFrom \/ vTo ~ Pred vFrom)
+                   )
+                   \/
+                   ( (hTo ~ hFrom \/ hTo ~ Succ hFrom \/ hTo ~ Pred hFrom)
+                   , (vTo ~ Succ vFrom \/ vTo ~ Pred vFrom)
+                   )
+                 )
+              => Unthreatened' kingCell (Opponent colour) facts'
+              -> Proxy opponentPiece
+              -> Move colour moveFrom moveTo facts facts'
+
 makeMove :: Move colour moveFrom moveTo facts facts' -> Board facts -> Board facts'
 makeMove !_ Board = Board
 
@@ -149,123 +189,3 @@ data Threatened' (moveTo :: Cell) (by :: Colour) (facts :: FactSet)
          (Move by moveFrom moveTo facts facts')
 
 type role Unthreatened' nominal nominal nominal
-
-movePawn1 :: forall (colour :: Colour)
-          -> forall (moveFrom :: Cell)
-          -> forall (moveTo :: Cell)
-          -> forall (kingCell :: Cell)
-          -> ( Holds (HasPiece Pawn colour moveFrom) facts
-             , Holds (IsEmpty moveTo) facts
-             , moveFrom ~ 'Cell hFrom vFrom
-             , moveTo ~ 'Cell hFrom vTo
-             , vTo ~ Forward colour vFrom
-             , 'True ~ If (colour == White)
-                 (vTo < MaxBound vFrom)
-                 (vTo > MinBound vFrom)
-             , facts' ~ DeleteInsert
-                 [IsEmpty moveTo, HasPiece Pawn colour moveFrom]
-                 [IsEmpty moveFrom, HasPiece Pawn colour moveTo]
-                 facts
-             , Holds (HasKing colour kingCell) facts
-             , Unthreatened kingCell (Opponent colour) facts'
-             )
-           => Board facts
-           -> Board facts'
-movePawn1 _ _ _ _ Board = Board
-
-movePawn2 :: forall (colour :: Colour)
-          -> forall (moveFrom :: Cell)
-          -> forall (moveTo :: Cell)
-          -> forall (kingCell :: Cell)
-          -> ( Holds (HasPiece Pawn colour moveFrom) facts
-             , Holds (IsEmpty moveTo) facts
-             , moveFrom ~ 'Cell hFrom (PawnRank colour)
-             , moveTo ~ 'Cell hFrom vTo
-             , vTo ~ Forward colour (Forward colour (PawnRank colour))
-             , facts' ~ DeleteInsert
-                 [IsEmpty moveTo, HasPiece Pawn colour moveFrom]
-                 [IsEmpty moveFrom, HasPiece Pawn colour moveTo]
-                 facts
-             , Holds (HasKing colour kingCell) facts
-             , Unthreatened kingCell (Opponent colour) facts'
-             )
-           => Board facts
-           -> Board facts'
-movePawn2 _ _ _ _ Board = Board
-
-capturePawn :: forall (colour :: Colour)
-            -> forall (moveFrom :: Cell)
-            -> forall (moveTo :: Cell)
-            -> forall (kingCell :: Cell)
-            -> forall (opponentPiece :: Piece)
-            -> ( Holds (HasPiece Pawn colour moveFrom) facts
-               , Holds (HasPiece opponentPiece (Opponent colour) moveTo) facts
-               , moveFrom ~ 'Cell hFrom vFrom
-               , moveTo ~ 'Cell hTo vTo
-               , vTo ~ Forward colour vFrom
-               -- Note that we now use an explicit disjunction constraint
-               -- (a, b \/ c) => t
-               -- instead of
-               -- a => (b => t, c => t)
-               -- as in Frog
-               , hTo ~ Rightward hFrom \/ hTo ~ Leftward hFrom
-               , facts' ~ DeleteInsert
-                   [HasPiece opponentPiece (Opponent colour) moveTo, HasPiece Pawn colour moveFrom]
-                   [IsEmpty moveFrom, HasPiece Pawn colour moveTo]
-                   facts
-               , Holds (HasKing colour kingCell) facts
-               , Unthreatened kingCell (Opponent colour) facts'
-               )
-             => Board facts
-             -> Board facts'
-capturePawn _ _ _ _ _ Board = Board
-
-moveKing :: forall (colour :: Colour)
-         -> forall (moveFrom :: Cell)
-         -> forall (moveTo :: Cell)
-         -> ( Holds (HasKing colour moveFrom) facts
-            , Holds (IsEmpty moveTo) facts
-            , moveFrom ~ 'Cell hFrom vFrom
-            , moveTo ~ 'Cell hTo vTo
-            , facts' ~ DeleteInsert
-                [IsEmpty moveTo, HasKing colour moveFrom]
-                [IsEmpty moveFrom, HasKing colour moveTo]
-                facts
-            , ( (hTo ~ Succ hFrom \/ hTo ~ Pred hFrom)
-              , (vTo ~ vFrom \/ vTo ~ Succ vFrom \/ vTo ~ Pred vFrom)
-              )
-              \/
-              ( (hTo ~ hFrom \/ hTo ~ Succ hFrom \/ hTo ~ Pred hFrom)
-              , (vTo ~ Succ vFrom \/ vTo ~ Pred vFrom)
-              )
-            , Unthreatened moveTo (Opponent colour) facts'
-            )
-         => Board facts
-         -> Board facts'
-moveKing _ _ _ Board = Board
-
-captureKing :: forall (colour :: Colour)
-            -> forall (moveFrom :: Cell)
-            -> forall (moveTo :: Cell)
-            -> forall (opponentPiece :: Piece)
-            -> ( Holds (HasKing colour moveFrom) facts
-               , Holds (HasPiece opponentPiece (Opponent colour) moveTo) facts
-               , moveFrom ~ 'Cell hFrom vFrom
-               , moveTo ~ 'Cell hTo vTo
-               , facts' ~ DeleteInsert
-                   [HasPiece opponentPiece (Opponent colour) moveTo, HasKing colour moveFrom]
-                   [IsEmpty moveFrom, HasKing colour moveTo]
-                   facts
-               , ( (hTo ~ Succ hFrom \/ hTo ~ Pred hFrom)
-                 , (vTo ~ vFrom \/ vTo ~ Succ vFrom \/ vTo ~ Pred vFrom)
-                 )
-                 \/
-                 ( (hTo ~ hFrom \/ hTo ~ Succ hFrom \/ hTo ~ Pred hFrom)
-                 , (vTo ~ Succ vFrom \/ vTo ~ Pred vFrom)
-                 )
-               , ReallyUnthreatened moveTo (Opponent colour) facts'
-               )
-             => Board facts
-             -> Board facts'
-captureKing _ _ _ _ Board = Board
-
