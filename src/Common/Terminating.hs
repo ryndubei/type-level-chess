123493140c46e8aaf0d3f3b048bab386d2c71fee
ryndubei <114586905+ryndubei@users.noreply.github.com>
2024-11-18 21:16:43 +0000

    Use de Bruijin indices for TermVar

--- src/Common/Terminating.hs
+++ src/Common/Terminating.hs
@@ -6,20 +6,21 @@
 {-# LANGUAGE RequiredTypeArguments #-}
 {-# LANGUAGE QuantifiedConstraints #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE TypeOperators #-}
 module Common.Terminating (type (-|>)(..), TermExpr(..), betaReduce, reduceCase) where
 
 import qualified Control.Category
-import GHC.TypeLits
 import Data.Kind
 import Data.Type.Equality
+import Data.List.Singletons (type (++))
 
-data TermVar (n :: Symbol) t = TermVar
+data TermVar (ctx :: [Type]) (t :: Type) where
+  TermVarNil :: TermVar (t ': ts) t
+  TermVarSucc :: TermVar ts t -> TermVar (t' ': ts) t
 
 data TermLit t where
   TermLitBool :: Bool -> TermLit Bool
   TermLitEq :: (a :~: b) -> TermLit (a :~: b)
-
-type TypeContext = [(Symbol, Type)]
 
 type family Lookup (x :: k) (xs :: [(k, v)]) :: Maybe v where
   Lookup k '[] = Nothing
@@ -29,25 +30,34 @@ type family Lookup (x :: k) (xs :: [(k, v)]) :: Maybe v where
 -- | Grammar for a simply-typed lambda calculus with `case` and literals.
 --
 -- Strict, so either bottom or finite.
-data TermExpr (c :: Constraint) (ctx :: TypeContext) t where
-  TermLamE :: (Lookup n ctx ~ Nothing) => TermVar n t -> (TermExpr c ('(n, a) ': ctx) b) -> (TermExpr c ctx (a -> b))
-  TermVarE :: (Lookup n ctx ~ Just t) => TermVar n t -> TermExpr c ctx t
+data TermExpr (c :: Constraint) (ctx :: [Type]) t where
+  TermLamE :: (TermExpr c (ctx ++ '[a]) b) -> (TermExpr c ctx (a -> b))
+  TermVarE :: TermVar ctx t -> TermExpr c ctx t
   TermAppE :: TermExpr c ctx (a -> b) -> TermExpr c ctx a -> TermExpr c ctx b
-  TermFreeE :: TermExpr c '[] a -> TermExpr c ctx' a
   TermCoerceE :: (c => t ~ t') => TermExpr c ctx t' -> TermExpr c ctx t
   TermLitE :: TermLit t -> TermExpr c ctx t
   TermCaseE :: TermExpr c ctx matchTy -> TermCase c ctx resultTy matchTy -> TermExpr c ctx resultTy
 
-data TermCase (c :: Constraint) (ctx :: TypeContext) (resultTy :: Type) (matchTy :: Type) :: Type where
+weaken :: TermExpr c ctx t -> TermExpr c (t' ': ctx) t
+weaken e = case e of
+  TermLamE e' -> TermLamE (weaken e')
+  TermVarE x -> TermVarE (TermVarSucc x)
+  TermAppE f e' -> TermAppE (weaken f) (weaken e')
+  TermCoerceE e' -> TermCoerceE (weaken e')
+  TermLitE l -> TermLitE l
+  TermCaseE m c -> TermCaseE (weaken m) $ case c of
+    TermCaseBool e1 e2 -> TermCaseBool (weaken e1) (weaken e2)
+    TermCaseEq e1 -> TermCaseEq (weaken e1)
+
+data TermCase (c :: Constraint) (ctx :: [Type]) (resultTy :: Type) (matchTy :: Type) :: Type where
   TermCaseBool :: TermExpr c ctx result -> TermExpr c ctx result -> TermCase c ctx result Bool
   TermCaseEq :: (TermExpr ((a ~ b), c) ctx result) -> TermCase c ctx result (a :~: b)
 
 constrain :: (c' => c) => TermExpr c ctx t -> TermExpr c' ctx t
 constrain e = case e of
-  TermLamE x e' -> TermLamE x (constrain e')
+  TermLamE e' -> TermLamE (constrain e')
   TermVarE x -> TermVarE x
   TermAppE f e' -> TermAppE (constrain f) (constrain e')
-  TermFreeE e' -> TermFreeE (constrain e')
   TermCoerceE e' -> TermCoerceE (constrain e')
   TermLitE l -> TermLitE l
   TermCaseE m c ->
@@ -59,11 +69,10 @@ constrain e = case e of
 betaReduce :: c => TermExpr c '[] (a -> b) -> TermExpr c '[] a -> TermExpr c '[] b
 betaReduce f e1 =
   case f of
-    TermLamE x e2 -> undefined
+    TermLamE e2 -> undefined
     TermAppE f' e2 ->
       let f'' = betaReduce f' e2
        in betaReduce f'' e1
-    TermFreeE f' -> betaReduce f' e1
     TermCaseE m c -> betaReduce (reduceCase m c) e1
     TermCoerceE f' -> betaReduce f' e1
 
@@ -79,16 +88,14 @@ reduceCase m c = case m of
   TermLitE (TermLitEq Refl) -> case c of
     TermCaseEq e' -> constrain e'
   TermCaseE m' c' -> reduceCase (reduceCase m' c') c
-  TermFreeE m' -> reduceCase m' c
   TermCoerceE m' -> reduceCase m' c
-  TermLamE _ _ -> case c of
+  TermLamE _ -> case c of
 
 -- | Terminating function type
 newtype a -|> b = TermFn (TermExpr () '[] (a -> b))
 
 instance Control.Category.Category (-|>) where
-  id = let x = TermVar @"x" in TermFn $ TermLamE x (TermVarE x)
+  id = TermFn $ TermLamE (TermVarE TermVarNil)
   (.) (TermFn f) (TermFn g) =
-    let x = TermVar @"x"
-     in TermFn $ TermLamE x (TermAppE (TermFreeE f) (TermAppE (TermFreeE g) (TermVarE x)))
+    TermFn $ TermLamE (TermAppE (weaken f) (TermAppE (weaken g) (TermVarE TermVarNil)))
 
